b7b4dd299d21a24cfd0dfb1074e9d16058672f58
omkarshete <sheteomkar104@gmail.com>
2021-11-23 23:05:07 +0530

    To-do_List_App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import react, { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+
+  //To add a task in a todo list and set the input field back to null
+  const addTask = () => {
+      setData([...data, task])
+      clearData.current.value = ""
+      setTask("")
+  }
+
+
+  //To delete a task
+  const deleteTask = (val) => {
+
+    setData(data.filter((task) => {
+        return task !== val
+      })
+    )
+
+
+  }
+  //States
+  const [data, setData] = useState([])
+  const [task, setTask] = useState("")
+  const clearData = useRef(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <h1>To-Do App</h1>
+    <div>
+        <input ref={clearData} type="text" placeholder="Add task" 
+        onChange={event => setTask(event.target.value)}
+        onKeyDown={(event) => {if(event.keyCode === 13)  addTask() } } />
+        <button className="add_task_btn"
+        onClick={addTask}  >Add task</button>
     </div>
-  );
+    <br />
+    <hr></hr>
+
+          <div className="todos">
+            <ul>
+              {  
+                data.map((value,key) => {
+                return (
+                <div className="deleteDiv">
+               
+                <li key = {key}>{value}</li>
+                <div>
+                <button className="deleteBtn" onClick={() => deleteTask(value)}>Delete Task</button>
+                </div>
+                </div>)}) 
+              }
+
+            </ul>
+          </div>
+
+
+    </div>
+  )
 }
 
 export default App;
